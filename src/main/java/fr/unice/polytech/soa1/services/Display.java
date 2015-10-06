1762ad39e7a4a5cf4a479e44267f77fa1f2f567c
rémy dupanloup <dupanloup.remy@etu.unice.fr>
2015-10-06 12:12:31 +0200

    ajout d'une methode

--- src/main/java/fr/unice/polytech/soa1/services/Display.java
+++ src/main/java/fr/unice/polytech/soa1/services/Display.java
@@ -1,11 +1,12 @@
 package fr.unice.polytech.soa1.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.unice.polytech.soa1.domain.Order;
 import fr.unice.polytech.soa1.domain.Storage;
 import org.json.JSONArray;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -26,10 +27,27 @@ public class Display {
     }
 
     @Path("/bulbs")
+    @GET
     public Response getNbBulb(){
         String answer = "{'numberOfBulb' : '";
         answer += Storage.getTotalStock();
         answer += "'}";
+        return Response.ok().entity(answer).build();
+    }
+
+    @Path("/order/{userId}/{orderId}")
+    @GET
+    public Response getSpecificOrder(@PathParam("userId") String userId,
+                                     @PathParam("orderId") String orderId){
+
+        Order o = Storage.getOrder(Integer.parseInt(orderId), Integer.parseInt(userId));
+        ObjectMapper mapper = new ObjectMapper();
+        String answer;
+        try {
+            answer = mapper.writeValueAsString(o);
+        } catch (JsonProcessingException json){
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
         return Response.ok().entity(answer).build();
     }
 }
